855d9c33c017e8dc8c940c94b42ce334238c4aa8
pmcclellan1289 <pmcclellan1289@gmail.com>
2019-08-12 10:47:38 -0500

    Phase 7 web scraper solution - Expanding to full implementation

--- src/main/java/com/CurrencyConversion/CurrWebScraper.java
+++ src/main/java/com/CurrencyConversion/CurrWebScraper.java
@@ -21,49 +21,22 @@ class CurrWebScraper implements CurrencyInterface { //TODO in progress
     }
 
     public Currency loadCurrency(String abbrev) {
-        initializeJSONString();
-
-        JSONArray jsonArray = new JSONArray(jsonString);
-        JSONObject thisObject = jsonArray.getJSONObject(0);
-        Object rates = thisObject.get("rates");
-        String rateString = "["+rates.toString()+"]";
-
-        //have to do this twice since it's a nested json
-        JSONArray desiredArray = new JSONArray(rateString);
+        initializeJSON();
+        JSONArray desiredArray = new JSONArray(getRateString());
         JSONObject desiredObject = desiredArray.getJSONObject(0);
         Object actualRates = desiredObject.get(abbrev.toUpperCase().replaceAll("\\s", ""));
 
         Double rate = (Double) actualRates;
-        Currency returnCurrency = new Currency();
-        returnCurrency.setAbbrev(abbrev);
-        returnCurrency.setRate(rate);
-        return returnCurrency;
+        return new Currency(abbrev,rate);
     }
-
-    public String listCurrencies() {
-        return null;
-    }
-
-    public void removeCurrency(Currency currency) {
-
-    }
-
-    public void update() { }
     
     static void populateDB() {  //This is for the database implementation
-        initializeJSONString();
-
-        JSONArray jsonArray = new JSONArray(jsonString);
-        JSONObject thisObject = jsonArray.getJSONObject(0);
-        Object rates = thisObject.get("rates");
-        String rateString = "["+rates.toString()+"]";
-
-        //have to do this twice, the JSON is nested
-        JSONArray thisArray = new JSONArray(rateString);
+        initializeJSON();
+        JSONArray thisArray = new JSONArray(getRateString());
         JSONObject desiredObject = thisArray.getJSONObject(0);
         Iterator keyList = desiredObject.keys();
-        CurrencyInterface dbConn = new DatabaseConnection();
 
+        CurrencyInterface dbConn = new DatabaseConnection();
         while (keyList.hasNext()) {
             String abbrev = keyList.next().toString();
             Object actualRates = desiredObject.get(abbrev);
@@ -76,7 +49,26 @@ class CurrWebScraper implements CurrencyInterface { //TODO in progress
         }
     }
 
-    private static void initializeJSONString() {
+
+
+    public String listCurrencies() {
+        initializeJSON();
+
+        return null;
+    }
+
+    public void removeCurrency(Currency currency) {
+        initializeJSON();
+
+    }
+
+    public void update() {
+        initializeJSON();
+
+
+    }
+
+    private static void initializeJSON() {
         try {
             URL url = new URL("https://api.exchangeratesapi.io/latest?base=USD");
             URLConnection conn = url.openConnection();
@@ -86,5 +78,11 @@ class CurrWebScraper implements CurrencyInterface { //TODO in progress
         }catch(IOException e){
             System.out.println("getValues() exception: (probably offline) " + e);
         }
+    }
+    private static String getRateString() {
+        JSONArray jsonArray = new JSONArray(jsonString);
+        JSONObject thisObject = jsonArray.getJSONObject(0);
+        Object rates = thisObject.get("rates");
+        return "["+rates.toString()+"]";
     }
 }
